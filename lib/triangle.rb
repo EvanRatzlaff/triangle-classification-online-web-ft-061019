bd202971bdd443d67d59da28b88dea6f0fc26fe7
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-08 16:22:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,10 @@ attr_accessor :length1, :length2, :length3
   end
   
   def validate_triangle 
-    
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+
   end 
 
   # def triangle2 
