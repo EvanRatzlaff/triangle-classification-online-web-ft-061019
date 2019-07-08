6b1c7c6317c47f980e6c398425272d518a120c07
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-08 16:14:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,9 +14,9 @@ attr_accessor :length1, :length2, :length3
     if length1 == length2 && length2 == length3 && length1 == length3
       :equilateral
     elsif length1 != length2 && length2 != length3 && length3 != length1
-      :scalene 
+      :isosceles 
     else 
-      :isosceles
+      :scalene
     end 
   
   end
