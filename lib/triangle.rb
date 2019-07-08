b6782d16a51058202c72c084ba4fbb89f07bc255
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-08 16:05:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,6 +19,7 @@ attr_accessor :length1, :length2, :length3
     else 
       :isosceles
     end 
+    binding.pry
   end
   
   def triangle 
@@ -27,7 +28,6 @@ attr_accessor :length1, :length2, :length3
 
   def triangle2 
     length1 + length2 > length3 && length1 + length3 > length2 && length2 + length3 > length1
-binding.pry    
   end 
   class TriangleError < StandardError 
   
